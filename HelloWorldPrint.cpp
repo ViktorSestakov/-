604c265f03c3722682b26007882293c126b14db9
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2025-09-18 20:12:51 +0500

    upload

---
+++ HelloWorldPrint.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void HelloWorld(string s) {
+        cout << s << endl;
+}
+
+int main() {
+        string print = "Hello, World!";
+        HelloWorld(print);
+}
