f05af3b6dda54fe5eecc0879430bb2e9ab2cbbaa
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2025-09-18 20:14:22 +0500

    Add files via upload

--- New Folder/1.cpp
+++ New Folder/1.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main() {
         setlocale(LC_ALL, "Russian");
-        
-        int sum = 0;
-        int array[5] = {1,1,1,1,1};
 
-        for(int i = 0; i < 5; i++) {
-                sum += array[i];
-        }
-        cout << sum;
+        cout << "Hello, World";
 }
